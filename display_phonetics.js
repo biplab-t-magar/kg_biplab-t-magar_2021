5f746597f95694b5c1bf76d39eab0300e077ae37
Biplab Thapa Magar <biplab.t.magar@gmail.com>
2021-05-03 14:41:51 -0400

    implemented function to convert each number to phonetics equivalent

--- display_phonetics.js
+++ display_phonetics.js
@@ -1,30 +1,42 @@
 function digit_to_word (digit) {
     switch(digit) {
-        case 0: 
+        case '0' || 0 : 
             return 'Zero';
-        case 1:
+        case '1' || 1:
             return 'One';
-        case 2:
+        case '2' || 2:
             return 'Two';
-        case 3:
+        case '3' || 3:
             return 'Three';
-        case 4:
+        case '4' || 4:
             return 'Four';
-        case 5:
+        case '5' || 5:
             return 'Five';
-        case 6:
+        case '6' || 6:
             return 'Six';
-        case 7:
+        case '7' || 7:
             return 'Seven';
-        case 8:
+        case '8' || 8:
             return 'Eight';
-        case 9:
+        case '9' || 9:
             return 'Nine';
+        default:
+            return '';
     };
 }
+
+function number_to_phonetic(num) {
+    let phonetic = '';
+    String(num).split('').forEach(digit => {
+        phonetic += digit_to_word(digit);
+    });
+    return phonetic;
+}
+
+
 function display_phonetics(args) {
     args.forEach(e => {
-        
+        e.split()
     });
 }
 
